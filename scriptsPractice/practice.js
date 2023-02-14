3387e2f6306cb334fcfa8f1dde72304552bd6c68
Brian Thomas <59669140+BFWThomas@users.noreply.github.com>
2023-02-14 11:59:39 -0600

    Added Player Renderer

--- scriptsPractice/practice.js
+++ scriptsPractice/practice.js
@@ -1,11 +1,31 @@
 var game;
 var playerCount = 0;
 
+function renderPlayers(game) {
+    const playersDiv = document.getElementById("players");
+    playersDiv.innerHTML = ""; // clear existing player divs
+  
+    game.players.forEach((player, index) => {
+      const playerDiv = document.createElement("div");
+      if (index == 0) {
+        playerDiv.innerText = 'Dealer';
+      } else if (index == 1){
+        playerDiv.innerText = `You: ${game.bet}`;
+        playerDiv.setAttribute('id', 'user-player');
+      } else {
+        playerDiv.innerText = `Player ${index + 1}`;
+      }
+      
+      playersDiv.appendChild(playerDiv);
+    });
+}
+
 // Button handling
 window.onload=function() {
     // Create a game instance
     document.getElementById("start").addEventListener('click', function() {
         game = new Blackjack
+        renderPlayers(game);
         game.deck.shuffle();
         game.bet = document.getElementById("bet").value;
     });
@@ -13,7 +33,8 @@ window.onload=function() {
     // Add player
     document.getElementById("addplayer").addEventListener('click', function() {
         game.addPlayer();
-        playerCount += 1
+        renderPlayers(game);
+        playerCount += 1;
     });
 
     // Update bet amounts
@@ -22,6 +43,7 @@ window.onload=function() {
             console.log("Round in play, bet cannot be changed");
         } else {
             game.bet = document.getElementById("bet").value;
+            document.getElementById("user-player").innerText = `You: ${game.bet}`;
         }
     });
 
