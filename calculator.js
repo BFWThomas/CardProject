83bae662a21194fed737b1ca98fb6d7f662f3860
Brian <thomasb3@oregonstate.edu>
2023-02-21 02:56:07 -0600

    Calculate bank value after basic play and ouput

--- calculator.js
+++ calculator.js
@@ -4,12 +4,23 @@ window.onload=function() {
     let bank;
     let bet;
     let numHands;
+    
     submit.onclick = function() {
+        // Get required values
         bank = document.getElementById("bankStart").value;
         bet = document.getElementById("bet").value;
         numHands = document.getElementById("hands").value;
-    }
 
+        //Calculate
+        let eValue = bet * (1-houseEdge);
+        let handDelta = eValue - bet;
+        bank = bank + (handDelta * numHands);
+
+        const outputDiv = documet.getElementById("output");
+        outputDiv.innerHTMl = "";
+
+        outputDiv.innerHTML = `<span>Bank value after <b>${numHabds}</b> hands at $${bet} per play: ${bank}<span>`
+    }
 
     return
 }
