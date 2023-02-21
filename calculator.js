b01bc512a6abd46de8d670a7e03538b68d09c36c
Brian <thomasb3@oregonstate.edu>
2023-02-21 02:47:50 -0600

    Calculator now pulls in user values when submitted

--- calculator.js
+++ calculator.js
@@ -1,3 +1,15 @@
 window.onload=function() {
+    let submit = document.getElementById("calculate");
+    const houseEdge = 0.007;
+    let bank;
+    let bet;
+    let numHands;
+    submit.onclick = function() {
+        bank = document.getElementById("bankStart").value;
+        bet = document.getElementById("bet").value;
+        numHands = document.getElementById("hands").value;
+    }
+
+
     return
 }
