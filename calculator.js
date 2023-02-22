0017e596d822343244c0ad157d088fb4fbf14402
Brian Thomas <59669140+BFWThomas@users.noreply.github.com>
2023-02-21 18:04:40 -0600

    Added bet variance functionality

--- calculator.js
+++ calculator.js
@@ -1,8 +1,37 @@
+const oddVariance = {
+    '-1': [0.4518, -0.0126],
+    '0': [0.2592, -0.0011],
+    '1': [0.1075, 0.0047],
+    '2': [0.0750, 0.0102],
+    '3': [0.0364, 0.0157],
+    '4': [0.0283, 0.0219],
+    '5': [0.0136, 0.0273],
+    '6': [0.0113, 0.0344],
+    '7': [0.0054, 0.0401],
+    '8': [0.0046, 0.0477],
+    '9': [0.0022, 0.0523],
+    '10': [0.0019, 0.0605],
+    '11': [0.0009, 0.0636],
+    '12': [0.0007, 0.0743]
+  };
+
+
 window.onload=function() {
-    const houseEdge = 0.007;
+    let houseEdge = -0.007;
     let bank;
     let bet;
     let numHands;
+
+    let toggle = document.getElementById("enableCount");
+    const varsDiv = document.getElementById("countVars");
+    varsDiv.classList.add("hidden");
+    toggle.addEventListener('click', function() {
+        if (toggle.checked) {
+            varsDiv.classList.remove("hidden");
+        } else {
+            varsDiv.classList.add("hidden");
+        }
+    });
 
     let submit = document.getElementById("calculate");
     submit.addEventListener('click', function() {
@@ -11,15 +40,48 @@ window.onload=function() {
         bet = Number(document.getElementById("bet").value);
         numHands = Number(document.getElementById("hands").value);
 
-        //Calculate
-        let ev = bet * (1-houseEdge);
-        let handDelta = ev - bet;
-        bank += handDelta * numHands;
-        bank = bank.toFixed(2);
-
+        // Calculate
         const outputDiv = document.getElementById("output");
-        outputDiv.innerHTML = "";
+        if (toggle.checked) {
+            let deltaBet = document.getElementById("betVariance").value;
+            let minCount = document.getElementById("minCount").value;
 
+            let under = 0;
+            let undercount = 0;
+            let over = 0;
+            let overcount = 0;
+            for (let i = -1; i < minCount-1; i++) {
+                const [v1, v2] = oddVariance[i.toString()];
+                under += v1 * v2;
+                undercount++;
+            }
+
+            for (let i = minCount; i <=12; i++) {
+                const [v1, v2] = oddVariance[i.toString()];
+                over += v1 * v2;
+                overcount++;
+            }
+
+            const underOdds = under/(undercount);
+            const overOdds = over/(overcount);
+
+            let underEV = bet * (1+underOdds);
+            let overEV = deltaBet * (1+overOdds);
+            let handDelta = (underEV - bet) + (overEV - deltaBet);
+            console.log("OverEV: "+over);
+            console.log("underEV: "+underEV);
+            bank += handDelta*numHands;
+            bank = bank.toFixed(2);
+
+        } else {
+            let ev = bet * (1+houseEdge);
+            let handDelta = ev - bet;
+            bank += handDelta * numHands;
+            bank = bank.toFixed(2);
+        }
+
+        // Write output
+        outputDiv.innerHTML = "";
         outputDiv.innerHTML = `<span>Bank value after <b>${numHands}</b> hands at <b>$${bet}</b> per play: <b>${bank}</b><span>`
     });
 }
